98403957b190b3c5a12daa89cdf4f17ae73a9cef
Bharat Badyal <myprof219@gmail.com>
2024-02-04 22:14:18 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version
+
+//alert("welcome to JS")  --> this will not work because we are using node not browser
+
+console.log(3+
+    2);// we should not use these type of code because "Code Readbility" should be the top most priority while writing code.
+
+let age = 12;
+let loggedIn = null;
+let isTrue = false;
+
+/* 
+Primitive data types
+    - number ==> 2^52
+    - bigInt
+    - string ==> "" or ''
+    - boolean ==> true and flase
+    - null ==> standalone value. "while checking null's type in js using typeOf operator it gives 'object'"
+    - undefined ==> "undefined"
+    - symbol ==> will be in use when advance topics will come
+
+Non-Primitive
+    - object ==> will be discussed in future lectures in detail.
+*/
